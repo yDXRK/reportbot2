909b28bcbf70182a290babff34bd8082c8dc1256
yDXRK <104456514+yDXRK@users.noreply.github.com>
2024-06-26 04:31:09 -0700

    Add files via upload

---
+++ report/libs/logo.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+#!/usr/bin/env python3
+from colorama import Fore, Back, Style
+from random import choice
+
+logo = """
+               S628L REPORT BOT    """
+
+
+
+
+
+def print_logo():
+    print(Fore.RED + Style.BRIGHT + logo + Style.RESET_ALL + Style.BRIGHT +"\n")
+    print(Fore.YELLOW + "      TheDbSearcher Project: My telegram:@s628l "+ Style.RESET_ALL + Style.BRIGHT)
+    print(Style.RESET_ALL + Style.BRIGHT, Style.BRIGHT)
